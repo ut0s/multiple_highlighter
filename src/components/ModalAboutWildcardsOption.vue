fd726004455be6afaec3e4fcea90b2653f08c7be
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2023-12-31 23:53:17 +0900

    feat: support dark mode

--- src/components/ModalAboutWildcardsOption.vue
+++ src/components/ModalAboutWildcardsOption.vue
@@ -18,23 +18,24 @@ const isOpen = ref(false);
           justify-center
           bg-gray-700 bg-opacity-50
         ">
-        <div class="max-w-2xl p-6 bg-white rounded-md shadow-xl">
+        <div class="max-w-2xl p-6 bg-white rounded-md shadow-xl dark:bg-slate-800">
           <div class="flex flex-row">
-            <h3 class="flex text-lg font-bold underline decoration-[0.5rem] decoration-green-300 pb-6 text-slate-800">
+            <h3
+              class="flex text-lg font-bold underline decoration-[0.5rem] decoration-green-300 pb-6 text-slate-800 dark:text-slate-400">
               About wildcards option:
             </h3>
             <div class="flex-grow"></div>
             <icon-close-circle class="flex text-3xl text-slate-400" @click="isOpen = false" />
           </div>
-          <div class="mt-4 text-sm">
+          <div class="mt-4 text-sm dark:text-slate-400">
             <span>Set to any of the following string values:</span>
-            <div class="bg-slate-200">
+            <div class="bg-slate-200 dark:bg-slate-700">
               <li class="mt-2">"disabled": </li>
               <ul class="">
                 <li>Disable wildcard usage</li>
               </ul>
             </div>
-            <div class="bg-slate-200">
+            <div class="bg-slate-200 dark:bg-slate-700">
               <li class="mt-2">"enabled": </li>
               <ul class="">
                 <li>When searching for "lor?m", the "?" will match zero or one non-space character (e.g.
@@ -42,7 +43,7 @@ const isOpen = ref(false);
                   characters (e.g. "lorm", "loram", "lor123m", etc).</li>
               </ul>
             </div>
-            <div class="bg-slate-200">
+            <div class="bg-slate-200 dark:bg-slate-700">
               <li class="mt-2">"withSpaces": </li>
               <ul class="">
                 <li>When searching for "lor?m", the "?" will match zero or one space or non-space character
